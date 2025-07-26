6528aca8aa38d16800d0b684ea305678f92f86c6
Mahir Keran <mahir.keran@edu.fit.ba>
2025-07-27 01:59:04 +0200

    Fix: removed submodule reference and added eCommerce.API as a normal folder

---
+++ eCommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Models.Entities;
+using eCommerce.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(IProductService productService) : ControllerBase
+    {
+        private readonly IProductService _productService = productService;
+        [HttpGet]
+        public List<Product> GetList()
+        {
+            return _productService.GetList();
+        }
+    }
+}
---
+++ eCommerce.API/Program.cs
@@ -0,0 +1,42 @@
+
+using eCommerce.Models.Entities;
+using eCommerce.Services;
+using eCommerce.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IProductService, ProductService>();
+
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+
+
+app.UseHttpsRedirection();
+
+
+
+
+
+app.MapControllers();
+app.Run();
+
